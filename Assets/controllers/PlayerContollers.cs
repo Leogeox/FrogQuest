e5da8bf8f08efd3b523a7a5bcbb6773829d5b35c
Leogeox <leovginx@gmail.com>
2025-03-21 11:08:14 +0100

    Update

--- Assets/controllers/PlayerContollers.cs
+++ Assets/controllers/PlayerContollers.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerContollers : MonoBehaviour
 {
@@ -9,56 +10,169 @@ public class PlayerContollers : MonoBehaviour
     [Header("Wall")]
     private Rigidbody2D _rgbd2d;
     public float _distanceDW;
-    public LayerMask detectwall;
+    public LayerMask detectground;
 
     [Header("Move")]
     public float speedMove = 4;
 
     [Header("Jump")]
-    [SerializeField] private float _forcejump = 5;
-    [SerializeField] private int _limitJump = 2;
+    [SerializeField] public float forcejump = 10;
+    [SerializeField] public int limitJump = 2;
     private int _currentJump;
 
+    [Header("WallSliding")]
+    [SerializeField] public float wallSlidespeed = 0;
+    [SerializeField] public Transform wallCheckPoint;
+    [SerializeField] Vector2 wallCheckSize;
+    private bool isSliding;
+    private bool isOnWall;
+
+    [Header("WallJump")]
+    [SerializeField] public float wallJumpForce = 18;
+    [SerializeField] public float wallJumpDirection = -1f;
+    [SerializeField] Vector2 wallJumpAngle;
+    private int _currentWallJump;
+
+    [Header("Ladder")]
+    private float _vertical;
+    private bool _isLadder;
+    private bool _isClimbing;
+
+    [Header("WallJump")]
+    public Sprite[] syanding;
+    public Sprite[] crouching;
+    private BoxCollider _boxCollider;
     void Awake()
     {
         TryGetComponent(out _rgbd2d);
         TryGetComponent(out _collider2D);
         TryGetComponent(out _spriteRend);
         TryGetComponent(out _transform);
+        TryGetComponent(out _boxCollider);
     }
+
 
     void Update()
     {
         if (Input.GetButton("Horizontal"))
             Move();
 
-        if (Input.GetKeyDown(KeyCode.Space) && _currentJump < _limitJump)
+        if (Input.GetKeyDown(KeyCode.Space) && _currentJump < limitJump)
             Jump();
+
+        Slide();
+        WallJumping();
+
+        _vertical = Input.GetAxisRaw("Vertical");
+        if (_isLadder && Mathf.Abs(_vertical) > 0f)
+        {
+            _isClimbing = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isClimbing)
+        {
+            _rgbd2d.gravityScale = 0f;
+            _rgbd2d.linearVelocity = new Vector2(_rgbd2d.linearVelocity.x, _vertical * speedMove);
+        }
+        else
+        {
+            _rgbd2d.gravityScale = 3f;
+        }
     }
 
     void Move()
     {
         Vector3 direction = Input.GetAxis("Horizontal") * Vector2.right;
 
-        var hit = Physics2D.BoxCast(_transform.position, Vector2.one, 0, direction, _distanceDW, detectwall);
-        if (hit.collider != null)
+        var ground = Physics2D.BoxCast(_transform.position, Vector2.one, 0, direction, _distanceDW, detectground);
+        if (ground.collider != null)
             return;
 
         if (!Input.GetKey(KeyCode.LeftControl))
-            _spriteRend.flipX = Input.GetAxis("Horizontal") < 0;
+        {
+            if(Input.GetAxis("Horizontal") < 0)
+               _transform.rotation = Quaternion.Euler(0, 180, 0);
+            else
+                _transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        }
+        //_spriteRend.flipX = Input.GetAxis("Horizontal") < 0;
 
         _transform.position += Vector3.right * Input.GetAxisRaw("Horizontal") * speedMove * Time.deltaTime;
     }
 
+
     void Jump()
     {
-        _rgbd2d.velocity = Vector2.up * _forcejump;
+        _rgbd2d.linearVelocity = Vector2.up * forcejump;
         _currentJump++;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground") || collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
             _currentJump = 0;
+    }
+
+
+    void Slide()
+    {
+        Vector3 direction = Input.GetAxis("Horizontal") * Vector2.right;
+
+        var ground = Physics2D.BoxCast(_transform.position, Vector2.one, 0, direction, _distanceDW, detectground);
+        isOnWall = Physics2D.OverlapBox(wallCheckPoint.position, wallCheckSize, 0, detectground);
+
+        if (isOnWall && _rgbd2d.linearVelocity.y < 0)
+        {
+            isSliding = true;
+        }
+        else
+        {
+            isSliding = false;
+        }
+
+        if (isSliding)
+        {
+            _rgbd2d.linearVelocity = new Vector2(_rgbd2d.linearVelocity.x, wallSlidespeed);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(wallCheckPoint.position, wallCheckSize);
+    }
+
+
+    void WallJumping()
+    {
+        wallJumpDirection *= -1;
+
+        if (isSliding || isOnWall)
+        {
+            _rgbd2d.AddForce(new Vector2(wallJumpForce * wallJumpDirection * wallJumpAngle.x, wallJumpForce * wallJumpAngle.y), ForceMode2D.Impulse);
+            _currentWallJump++;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            _isLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            _isLadder = false;
+            _isClimbing = false;
+        }
     }
 }
